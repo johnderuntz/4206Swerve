a86cf04457b6086837686d5623af1237440fdcde
johnderuntz <44377280+johnderuntz@users.noreply.github.com>
2020-11-10 19:15:13 -0600

    added stuff

--- src/main/java/com/swervedrivespecialties/exampleswerve/subsystems/DrivetrainSubsystem.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/subsystems/DrivetrainSubsystem.java
@@ -21,13 +21,16 @@ import org.frcteam2910.common.robot.drivers.Mk2SwerveModuleBuilder;
 import org.frcteam2910.common.robot.drivers.NavX;
 
 public class DrivetrainSubsystem extends Subsystem {
-    private static final double TRACKWIDTH = 19.5;
-    private static final double WHEELBASE = 23.5;
+    private static final double TRACKWIDTH = 25.5;  //25.5
+    private static final double WHEELBASE = 20.5;     //21
 
-    private static final double FRONT_LEFT_ANGLE_OFFSET = -Math.toRadians(0.0);
-    private static final double FRONT_RIGHT_ANGLE_OFFSET = -Math.toRadians(0.0);
-    private static final double BACK_LEFT_ANGLE_OFFSET = -Math.toRadians(0.0);
-    private static final double BACK_RIGHT_ANGLE_OFFSET = -Math.toRadians(0.0);
+
+//neeeds to be more exact
+    private static final double FRONT_LEFT_ANGLE_OFFSET = -Math.toRadians(122); 
+    private static final double FRONT_RIGHT_ANGLE_OFFSET =  -Math.toRadians(17.4);  
+    private static final double BACK_LEFT_ANGLE_OFFSET = -Math.toRadians(170.6); 
+    private static final double BACK_RIGHT_ANGLE_OFFSET = -Math.toRadians(149.2); 
+    
 
     private static DrivetrainSubsystem instance;
 
